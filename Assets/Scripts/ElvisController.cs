c1ee718db45c5bacabe76af0281fb770f41a7358
HernadiMihaly <hernadi.mihaly99@gmail.com>
2022-11-03 13:43:55 +0100

    Adding a script

--- Assets/Scripts/ElvisController.cs
+++ Assets/Scripts/ElvisController.cs
@@ -8,31 +8,128 @@ using UnityEngine;
 
 public class ElvisController: Agent
 {
-    public float speed = 10.0f;
+    Rigidbody rigidBody;
+    public float speed = 5;
+
+    public Transform TargetTransform;
+
+    private GameObject goal;
+
+    [SerializeField]
+    private GameObject[] guitars = new GameObject[9];
+
+    [SerializeField] 
+    private GameObject fanGirl;
+
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnEpisodeBegin()
     {
+        transform.localPosition = new Vector3(0, 0, -3);
+
+        // Generate a random position for the treasure prefab 
+        float xPosition = UnityEngine.Random.Range(-3.75f, 5);
+
+        // Randomizing guitars' and equipments' positions
+        RandomizeGuitars();
+
+        //Placing fangirl to her base position
+        float decisionValue = UnityEngine.Random.Range(0, 2);
+       
+        if (decisionValue >= 1)
+        {
+            fanGirl.transform.localPosition = new Vector3(6.71f, 0, 0);
+        } else
+        {
+            fanGirl.transform.localPosition = new Vector3(-8.9f, 0, 0);
+        }
+
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        // The position of the agent
+        sensor.AddObservation(transform.localPosition.x);
+        sensor.AddObservation(transform.localPosition.y);
+
+        // The position of the treasure prefab
+        sensor.AddObservation(TargetTransform.localPosition.x);
+        sensor.AddObservation(TargetTransform.localPosition.y);
+
+        // The distance between the agent and the treasure
+        sensor.AddObservation(Vector3.Distance(TargetTransform.localPosition, transform.localPosition));
+    }
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        var actionTaken = actions.ContinuousActions;
+
+        float actionSpeed = (actionTaken[0] + 1) / 2; // [0, +1]
+        float actionSteering = actionTaken[1]; // [-1, +1]
+
+        transform.Translate(actionSpeed * Vector3.forward * speed * Time.deltaTime);
+        transform.rotation = Quaternion.Euler(new Vector3(0, actionSteering * 180, 0));
+
+        AddReward(-0.01f);
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        ActionSegment<float> actions = actionsOut.ContinuousActions;
+
+        actions[0] = -1;
+        actions[1] = 0;
+
         if (Input.GetKey("w"))
-            transform.Translate(Vector3.forward * speed * Time.deltaTime);
-        
+            actions[0] = 1;
+
         if (Input.GetKey("d"))
-            transform.Rotate(0.0f, +2f, 0.0f);
+            actions[1] = +0.5f;
 
         if (Input.GetKey("a"))
-            transform.Rotate(0.0f, -2f, 0.0f);
+            actions[1] = -0.5f;
     }
 
-    void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision collision)
     {
-       if (collision.collider.tag == "Wall"){
-       
-       }
+        if (collision.collider.tag == "Wall" || collision.collider.tag == "OtherEquipment" || collision.collider.tag == "FanGirl")
+        {
+            AddReward(-1);
+            EndEpisode();
+        }
+        else if (collision.collider.tag == "TargetGuitar")
+        {
+            AddReward(1);
+            EndEpisode();
+        }
     }
- 
+
+    private void RandomizeGuitars() {
+        float[] xPositions = {-3.79f, -2.63f, -1.421f, -0.319f, 1.01f, 2.14f, 3.24f, 4.34f, 5.4274f};
+        xPositions = ShuffleArray<float>(xPositions);
+        
+
+        for (int i=0; i<xPositions.Length; i++)
+        {
+            guitars[i].transform.localPosition = new Vector3(xPositions[i], 2.25f, 4.77f);
+        }
+    }
+
+    private T[] ShuffleArray<T>(T[] array)
+    {
+        System.Random r = new System.Random();
+        for (int i = array.Length; i > 0; i--)
+        {
+            int j = r.Next(i);
+            T k = array[j];
+            array[j] = array[i - 1];
+            array[i - 1] = k;
+        }
+
+        return array;
+    }
+
 }
---
+++ Assets/Scripts/WayPointFollower.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WayPointFollower : MonoBehaviour
+{
+    [SerializeField] GameObject[] waypoints;
+    int currentWaypointIndex = 0;
+
+    [SerializeField] float speed = 3;
+
+  
+    void Update()
+    {
+        
+        
+            if (Vector3.Distance(transform.localPosition, waypoints[currentWaypointIndex].transform.localPosition) < .1f)
+            {
+
+                
+            transform.Rotate(Vector3.up, -180f);
+                
+            currentWaypointIndex++;
+                if (currentWaypointIndex >= waypoints.Length)
+                {
+                    currentWaypointIndex = 0;
+                }
+            }
+
+
+            transform.localPosition = Vector3.MoveTowards(transform.localPosition, waypoints[currentWaypointIndex].transform.localPosition, speed * Time.deltaTime);
+
+        
+
+    }
+
+   
+}
